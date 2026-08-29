beef695690f6966f6e1250317e8e83c3625edd92
berno <berno.binkert@gmail.com>
2026-08-29 18:40:06 +0200

    implemented docker secrets for db request

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 import mysql.connector
+import os
+import hvac
 
 app = FastAPI()
 
+def read_secret(name: str) -> str:
+    with open(F"/run/secrets/{name}", "r") as f:
+        return f.read().strip()
+
 def get_db():
+    password = read_secret("db_user_password")
+
     return mysql.connector.connect(
         host="db",
         user="hello",
-        password="hellopassword",
+        password=password,
         database="helloapp",
     )
-
 
 @app.get("/")
 def root():
